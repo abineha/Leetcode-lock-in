d8612d4be8e438a081cac6f109ecdd8c27f7ac7a
Abineha Prabu <101180980+abineha@users.noreply.github.com>
2025-06-01 21:44:49 +0530

    85 qs

---
+++ neetcode/roadmap/85_subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        result = []
+        subset = []     # current subset we building
+
+        def dfs(i):     # ith element aka current index we're looking at
+            if i >= len(nums):
+                result.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i+1)
+
+            # decision not to include nums[i]
+            subset.pop()
+            dfs(i+1)
+
+        dfs(0)
+        return result
