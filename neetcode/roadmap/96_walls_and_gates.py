7181461922f4d585301b5ce263330186fccf38cb
Abineha Prabu <101180980+abineha@users.noreply.github.com>
2025-06-03 20:10:47 +0530

    96 qs

---
+++ neetcode/roadmap/96_walls_and_gates.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def wallsAndGates(self, rooms: list[list[int]]) -> None:
+        ROWS, COLS = len(rooms), len(rooms[0])
+        visit = set()
+        q = deque() 
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append([r,c])
+                    visit.add((r,c))
+
+        def addroom(r, c):
+            if r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or rooms[r][c] == -1:
+                return
+            visit.add((r,c))
+            q.append([r,c])
+
+        dist = 0
+        while q:
+            for _ in range(len(q)):
+                r, c = q.popleft()  # BFS
+                rooms[r][c] = dist  # for each gate
+
+                addroom(r+1, c)
+                addroom(r-1, c)
+                addroom(r, c+1)
+                addroom(r,c-1)
+            dist += 1
