6eeec45a5cc8fbbe99e932def307526afdf4ba33
Abineha Prabu <101180980+abineha@users.noreply.github.com>
2025-08-06 19:40:58 +0530

    11 qs

---
+++ neetcode/Leetcode all/Trees/11_create_binary_tree_from_description.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        nodes = {}
+        children = set()
+
+        for parent, child, left in descriptions:
+            children.add(child)
+            if parent not in nodes:
+                nodes[parent] = TreeNode(parent)
+            if child not in nodes:
+                nodes[child] = TreeNode(child)
+            if left:
+                nodes[parent].left = nodes[child]
+            else:
+                nodes[parent].right = nodes[child]
+        
+        for p, c, l in descriptions:
+            if p not in children:
+                return nodes[p]
