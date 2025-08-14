43dfd37e51cff2bdc144bd1e10f22b19972eb595
Abineha Prabu <101180980+abineha@users.noreply.github.com>
2025-08-14 11:42:57 +0530

    20 qs

---
+++ neetcode/Leetcode all/Trees/20_kth_largest_sum_in_a_binary_tree.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from collections import deque
+import heapq
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        q = deque([root])
+        min_heap = []   # size = k
+
+        while q:
+            lvl_sum = 0
+
+            for i in range(len(q)):
+                node = q.popleft()
+                lvl_sum += node.val
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            heapq.heappush(min_heap, lvl_sum)
+            if  len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        return -1 if len(min_heap) < k else min_heap[0]
