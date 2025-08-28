9b43b39b3307c95c88de7bb291b2751b31972d38
Abineha Prabu <101180980+abineha@users.noreply.github.com>
2025-08-29 00:44:24 +0530

    5,6 qs

---
+++ neetcode/Leetcode all/Tries/5_sum_of_prefix_scores_of_strings.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        result = []
+        count = defaultdict(int)
+
+        for w in words:
+            for i in range(len(w)):
+                count[w[:i+1]] += 1
+            
+        for w in words:
+            score = 0
+            for i in range(len(w)):
+                score += count[w[:i+1]]
+            result.append(score)
+        
+        return result
+    
+# TRIE
+
+class PrefixNode:
+    def __init__(self):
+        self.children = [None]*26
+        self.count = 0
+
+class PrefixTree:
+    def __init__(self):
+        self.root = PrefixNode()
+    
+    def insert(self, w):
+        cur = self.root
+        
+        for c in w:
+            i = ord(c)-ord('a')
+            if not cur.children[i]:
+                cur.children[i] = PrefixNode()
+            cur = cur.children[i]
+            cur.count += 1
+    
+    def get_score(self, w):
+        cur = self.root
+        score = 0
+
+        for c in w:
+            i = ord(c)-ord('a')
+            cur = cur.children[i]
+            score += cur.count
+        
+        return score
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        result = []
+        prefix_tree = PrefixTree()
+
+        for w in words:
+            prefix_tree.insert(w)
+        
+        for w in words:
+            result.append(prefix_tree.get_score(w))
+        
+        return result
